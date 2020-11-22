66709256e9aa9628984edd36be1d0206296dc219
Brayan Potosi <bp990308@hotmail.com>
2020-11-22 12:28:10 -0500

    Added statement to exercices

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,3 +1,17 @@
+# Exercise 3
+
+# Take a list, say for example this one:
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+
+# Extras
+
+# Instead of printing the elements one by one, make a new list that has all the elements less than 5
+# from this list in it and print out this new list.
+# Write this in one line of Python.
+# Ask the user for a number and return a list that contains only elements from the original list
+# a that are smaller than that number given by the user.
+
 def run():
 
     len_list = int(input('Introduzca la cantidad de elementos que tendra su lista : '))
@@ -11,10 +25,12 @@ def run():
             list_item = int(input(f'Intozuca el {i + 1} elemento : '))
             user_list.append(list_item)
 
+        print_list(user_list, num_user)
+
     else:
         print('Solo se aceptan numeros positivos')
 
-    print_list(user_list, num_user)
+
 
 
 def print_list(user_list, num_user):
